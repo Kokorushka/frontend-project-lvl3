ba803518df82b9c146a788822dc905f361320e0b
Alexandra <kokosha117@gmail.com>
2021-03-11 16:54:21 +0300

    webpack installed

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+
+module.exports = {
+  mode: 'development',
+  plugins: [new webpack.ProgressPlugin()],
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      include: [path.resolve(__dirname, 'src')],
+      loader: 'babel-loader'
+    }]
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost'
+  }
+}
