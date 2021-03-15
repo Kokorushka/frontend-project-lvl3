b5869c4c0dbe7b45845ac30e1bdd0ad403300726
Alexandra <kokosha117@gmail.com>
2021-03-15 18:30:33 +0300

    webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-
-
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   plugins: [new webpack.ProgressPlugin()],
 
   module: {
@@ -13,11 +11,41 @@ module.exports = {
       test: /\.(js|jsx)$/,
       include: [path.resolve(__dirname, 'src')],
       loader: 'babel-loader'
-    }]
+    },
+     {
+       test: /\.css$/,
+       use: ['style-loader', 'css-loader']
+     }
+    // {
+    //   test: /\.(scss)$/,
+    //   use: [{
+    //     loader: 'style-loader', // inject CSS to page
+    //   }, {
+    //     loader: 'css-loader', // translates CSS into CommonJS modules
+    //   }, {
+    //     loader: 'postcss-loader', // Run post css actions
+    //     options: {
+    //       plugins: function () { // post css plugins, can be exported to postcss.config.js
+    //         return [
+    //           require('precss'),
+    //           require('autoprefixer')
+    //         ];
+    //       }
+    //     }
+    //   }, {
+    //     loader: 'sass-loader' // compiles Sass to CSS
+    //   }]
+    // },
+   ]
   },
 
   devServer: {
     open: true,
     host: 'localhost'
-  }
+  },
+   plugins: [
+     new HtmlWebpackPlugin({
+       template: 'template.html',
+     }),
+   ],
 }
