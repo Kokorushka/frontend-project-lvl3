fefc9f7dc40659dd0224217fe638e2b25ee41453
Alexandra <kokosha117@gmail.com>
2021-06-15 11:49:44 +0300

    change validation url

--- utils/getValidatdUrl.js
+++ utils/getValidatdUrl.js
@@ -6,8 +6,8 @@ const getValidatedUrl = (url, urlList, instancei18n) => {
       matches: `${instancei18n.t('errors.incorrect')}`,
     },
   });
-  const schema = yup.string()
-    .matches(/(https?:\/\/)?([\w\W])*(rss)+([\w\W])*/)
+  const schema = yup.string().url()
+    // .matches(/(https?:\/\/)?([\w\W])*(rss)+([\w\W])*/)
     .notOneOf(urlList, `${instancei18n.t('errors.wasAddedBefore')}`);
   try {
     schema.validateSync(url, {
